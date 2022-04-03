f1bd0f12e7e99dd4916a5713b9c9ea3947ed3224
Radouane LAMJAHDA <Radouane LAMJAHDA@DESKTOP-RTS6F3I>
2022-04-03 19:44:58 +0000

    Second Commit

---
+++ src/client/MyClient.java
@@ -0,0 +1,30 @@
+package client;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+
+public class MyClient {
+
+	public static void main(String[] args) throws Exception {
+		System.out.println("Je me connete au serveur ");
+		java.net.Socket socket = new Socket("localhost",1234);
+		InputStream is=socket.getInputStream();
+		OutputStream os=socket.getOutputStream();
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Donner un nombre: ");
+		int nb = scanner.nextInt();
+		
+		System.out.println("J'envoie le nombre "+nb+" au serveur");
+		os.write(nb);
+		System.out.println("J'attend la réponse du serveur..");
+		int rep = is.read();
+		System.out.println("Réponse du serveur est "+rep);
+		
+
+	}
+
+}
